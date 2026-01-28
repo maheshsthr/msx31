08659e6e5386ff199dc3bf30ccf512dedd7ffe65
Mahesh Suthar <msx31.mahesh@gmail.com>
2026-01-28 19:10:46 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById("mob-menu");
-    menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
-}
-
-function hideMenu() {
-    document.getElementById("mob-menu").style.display = "none";
-}
